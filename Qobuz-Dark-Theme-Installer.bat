8f287f6657a0bb4b6f8725315f5fadfeb4bdcfcf
Evo-c <mark@evoc.me>
2020-07-24 14:44:09 +0100

    Changed how it handles updates and downloading
    Hopefully this sorts the issue with it caching hold versions and will always download file when ran.

--- Qobuz-Dark-Theme-Installer.bat
+++ Qobuz-Dark-Theme-Installer.bat
@@ -1,25 +1,45 @@
 @echo off
 rem Visit https://github.com/Evo-c/qobuz-dark-theme/ for updates
 title Qobuz dark theme installer
-set "QDTVER=1.0"
-if not exist "%~dp0style.css" (
-  echo.
-  echo Style.css not found in current folder... Will force update
-  set "QDTVER=0.1"
-)
 echo.
-echo Current version is %QDTVER%, checking for update...
-goto UpdateCheck
+set FORCEINS="false"
+goto Start
 
 :Start
 echo.
 echo Trying to find your Qobuz folder...
 FOR /F "usebackq delims=" %%p IN (`dir "C:\Users\%username%\AppData\Local\Qobuz" /b /ad ^| find "app-5"`) DO (
   set "QBPATH=%%p"
+  set "QDTVER=app-5.4.3-b006"
 )
 echo.
 echo Found %QBPATH%!
 echo.
+if "%QBPATH%"=="app-5.4.3-b006" (
+echo Version supported!
+set "URL=https://raw.githubusercontent.com/Evo-c/qobuz-dark-theme/master/style.css"
+goto Update
+)
+goto NotSupported
+
+:NotSupported
+cls
+color 0c
+echo Version not supported, applying the theme could cause bugs and may break the app!
+echo Enter yes to continue...
+echo.
+set /p forceinstall=""
+if "%forceinstall%"=="yes" (
+cls
+color 07
+set "URL=https://raw.githubusercontent.com/Evo-c/qobuz-dark-theme/master/style.css"
+goto Update
+)
+echo Aborted
+pause
+exit
+
+:Install
 if exist "C:\Users\%username%\AppData\Local\Qobuz\%QBPATH%\resources\app\node_modules\qobuz-dwp-ui\dist\style.css.old" (
   del "C:\Users\%username%\AppData\Local\Qobuz\%QBPATH%\resources\app\node_modules\qobuz-dwp-ui\dist\style.css.old"
 )
@@ -29,55 +49,34 @@ echo.
 echo.
 echo Success!
 echo.
-ping 127.0.0.1>nul -n 6
+ping 127.0.0.1>nul -n 5
 goto End
 
-:UpdateCheck
-set "URL=https://raw.githubusercontent.com/Evo-c/qobuz-dark-theme/master/version.txt"
-for %%a in (%URL%) do set "File=%%~nxa"
-set "DownloadFolder=%Temp%"
-:Process
+:Update
+set "DownloadFolder=%~dp0"
+set "File=style.css"
 Call :Download "%URL%" "%DownloadFolder%\%File%"
-if "%URL%"=="https://raw.githubusercontent.com/Evo-c/qobuz-dark-theme/master/version.txt" (
-  for /f "tokens=*" %%A in ('Type "%DownloadFolder%\%File%"') do (set "QDTNEWVER=%%A")
-)
-if "%QDTNEWVER%" NEQ "%QDTVER%" (
-  echo Version %QDTNEWVER% found
-  set "DownloadFolder=%~dp0"
-  set "File=style.css"
-  set "URL=https://raw.githubusercontent.com/Evo-c/qobuz-dark-theme/master/style.css"
-  set "QDTVER=%QDTNEWVER%"
-  set "QDTUPDATED=true"
-  goto Process
-)
-if "%QDTUPDATED%"=="true" (
-  goto Updated
-)
-if "%QDTNEWVER%"=="%QDTVER%" (
-  goto NoUpdate
-)
-rem ignore this, so it jumps over...
-if "one"=="two" (
-:Download <Url> <File>
-certutil.exe -urlcache -split -f %1 %2 >nullqdti
-)
-rem ignore this, so it jumps over...
-if "three"=="two" (
-:NoUpdate
-echo Latest version is %QDTNEWVER%, no update needed...
-goto Start
-)
-rem ignore this, so it jumps over...
-if "four"=="two" (
-:Updated
+goto Downloaded
+
+echo Error
+pause
+exit
+
+:Downloaded
+if exist "style.css" (
 echo Downloaded style.css for version %QDTVER%
-goto Start
+goto Install
+) else (
+echo "Download failed"
+pause
+exit
 )
-rem ignore this, so it jumps over...
-if "six"=="two" (
+
 :End
 if exist "%~dp0nullqdti" (
  del "%~dp0nullqdti"
 )
 exit
-)
+
+:Download <Url> <File>
+certutil.exe -urlcache -split -f %1 %2 >nullqdti
